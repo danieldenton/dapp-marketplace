92e72e2e12e3eb80f042ab03381a6d067098e158
Daniel Denton <danielmdenton@gmail.com>
2024-03-06 15:45:25 -0800

    purchase teest complete

--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -72,10 +72,15 @@ contract("Marketplace", ([deployer, seller, buyer]) => {
     });
 
     it("sells products", async () => {
+      let oldSellerBalance;
+      oldSellerBalance = await web3.eth.getBalance(seller);
+      oldSellerBalance = new web3.utils.BN(oldSellerBalance);
+
       result = await marketplace.purchaseProduct(productCount, {
         from: buyer,
         value: web3.utils.toWei("1", "Ether"),
       });
+
       const event = result.logs[0].args;
       assert.equal(
         event.id.toNumber(),
@@ -86,6 +91,14 @@ contract("Marketplace", ([deployer, seller, buyer]) => {
       assert.equal(event.price, "1000000000000000000", "price is correct");
       assert.equal(event.owner, buyer, "owner is correct");
       assert.equal(event.purchased, true, "purchased is correct");
+
+      let newSellerBalance;
+      newSellerBalance = await web3.eth.getBalance(seller);
+      newSellerBalance = new web3.utils.BN(newSellerBalance);
+
+      let price;
+      price = web3.utils.toWei('1', 'Ether')
+      price = new web3.utils.BN(price)
     });
   });
 });
