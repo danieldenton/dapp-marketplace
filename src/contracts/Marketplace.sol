343868074be0d0f7f95104e3b6c63ae2b195533c
Daniel Denton <danielmdenton@gmail.com>
2024-03-06 11:15:54 -0800

    tests 3

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.5.0;
 
 contract Marketplace {
     string public name;
+    uint public productCount = 0;
+    mapping(uint => Product) public products;
 
     struct Product {
         uint id;
@@ -11,7 +13,27 @@ contract Marketplace {
         bool purchased;
     }
 
+    event ProductCreated(
+        uint id,
+        string name,
+        uint price,
+        address owner,
+        bool purchased
+    );
+
     constructor() public {
         name = "Dapp University Marketplace";
+    }
+
+    function createProduct(string memory _name, uint _price) public {
+        require(bytes(name).length > 0);
+        require(_price > 0);
+        // Make sure params are correct
+        // Increment
+        productCount ++;
+        //  Create the product
+        products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+        // Trigger an event
+        emit ProductCreated(productCount, _name, _price, msg.sender, false);
     }
 }
